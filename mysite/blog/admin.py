a4c49d386273c737a023146283ed1b309a13a7e5
maks <maks20022409@gmail.com>
2024-01-03 14:21:56 +0200

    First migration and admin register

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post
 
 
+@admin.register(Post)
+class AdminPost(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']

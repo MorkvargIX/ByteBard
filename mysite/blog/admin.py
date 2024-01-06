6cefeb89f5024d881929919f5dd702a9a6553f88
maks <maks20022409@gmail.com>
2024-01-06 19:21:02 +0200

    Create model 'Comment' and add form.

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 
 @admin.register(Post)
@@ -10,3 +10,10 @@ class AdminPost(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     raw_id_fields = ['author']
     ordering = ['status', 'publish']
+
+
+@admin.register(Comment)
+class AdminComment(admin.ModelAdmin):
+    list_display = ['name', 'email', 'post', 'created', 'active']
+    list_filter = ['active', 'created', 'updated']
+    search_fields = ['name', 'email', 'body']

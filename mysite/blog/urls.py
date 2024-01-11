6df9663c389d2b28099e86182336a9461cf7481c
maks <maks20022409@gmail.com>
2024-01-11 15:09:24 +0200

    Comment template and views function

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('<slug:slug>/<int:id>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
 ]
